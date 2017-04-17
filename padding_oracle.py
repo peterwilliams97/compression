bb97951f4ccedc12779c733b15a178ae4b374ac5
Peter Williams <peter.williams@papercut.com>
2017-04-17 21:57:08 +1000

    padding oracle completed

--- padding_oracle.py
+++ padding_oracle.py
@@ -1,54 +1,4 @@
 """
-    ** 73 "s"
-    ** 7073 "ps"
-    ** 717073 "qps"
-    ** 76717073 "vqps"
-    ** 7776717073 "wvqps"
-    ** 747776717073 "twvqps"
-    ** 75747776717073 "utwvqps"
-    ** 7a75747776717073 "zutwvqps"
-    ** 7b7a75747776717073 "{zutwvqps"
-    ** 787b7a75747776717073 "x{zutwvqps"
-    ** 79787b7a75747776717073 "yx{zutwvqps"
-    ** 7e79787b7a75747776717073 "~yx{zutwvqps"
-    ** 7f7e79787b7a75747776717073 "~yx{zutwvqps"
-    ** 7c7f7e79787b7a75747776717073 "|~yx{zutwvqps"
-    ** 7d7c7f7e79787b7a75747776717073 "}|~yx{zutwvqps"
-    ** 627d7c7f7e79787b7a75747776717073 "b}|~yx{zutwvqps"
-
-    **  1: 73                                       "s"
-    **  2: 7073                                     "ps"
-    **  3: 717073                                   "qps"
-    **  4: 76717073                                 "vqps"
-    **  5: 7776717073                               "wvqps"
-    **  6: 747776717073                             "twvqps"
-    **  7: 75747776717073                           "utwvqps"
-    **  8: 7a75747776717073                         "zutwvqps"
-    **  9: 7b7a75747776717073                       "{zutwvqps"
-    ** 10: 787b7a75747776717073                     "x{zutwvqps"
-    ** 11: 79787b7a75747776717073                   "yx{zutwvqps"
-    ** 12: 7e79787b7a75747776717073                 "~yx{zutwvqps"
-    ** 13: 7f7e79787b7a75747776717073               "~yx{zutwvqps"
-    ** 14: 7c7f7e79787b7a75747776717073             "|~yx{zutwvqps"
-    ** 15: 7d7c7f7e79787b7a75747776717073           "}|~yx{zutwvqps"
-    ** 16: 627d7c7f7e79787b7a75747776717073         "b}|~yx{zutwvqps"
-    ** 17: 63627d7c7f7e79787b7a75747776717073       "cb}|~yx{zutwvqps"
-    ** 18: 6063627d7c7f7e79787b7a75747776717073     "`cb}|~yx{zutwvqps"
-    ** 19: 616063627d7c7f7e79787b7a75747776717073   "a`cb}|~yx{zutwvqps"
-    ** 20: 66616063627d7c7f7e79787b7a75747776717073 "fa`cb}|~yx{zutwvqps"
-    ** 21: 6766616063627d7c7f7e79787b7a75747776717073 "gfa`cb}|~yx{zutwvqps"
-    ** 22: 646766616063627d7c7f7e79787b7a75747776717073 "dgfa`cb}|~yx{zutwvqps"
-    ** 23: 65646766616063627d7c7f7e79787b7a75747776717073 "edgfa`cb}|~yx{zutwvqps"
-    ** 24: 6a65646766616063627d7c7f7e79787b7a75747776717073 "jedgfa`cb}|~yx{zutwvqps"
-    ** 25: 6b6a65646766616063627d7c7f7e79787b7a75747776717073 "kjedgfa`cb}|~yx{zutwvqps"
-    ** 26: 686b6a65646766616063627d7c7f7e79787b7a75747776717073 "hkjedgfa`cb}|~yx{zutwvqps"
-    ** 27: 69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "ihkjedgfa`cb}|~yx{zutwvqps"
-    ** 28: 6e69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "nihkjedgfa`cb}|~yx{zutwvqps"
-    ** 29: 6f6e69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "onihkjedgfa`cb}|~yx{zutwvqps"
-    ** 30: 6c6f6e69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "lonihkjedgfa`cb}|~yx{zutwvqps"
-    ** 31: 6d6c6f6e69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "mlonihkjedgfa`cb}|~yx{zutwvqps"
-    ** 32: 526d6c6f6e69686b6a65646766616063627d7c7f7e79787b7a75747776717073 "Rmlonihkjedgfa`cb}|~yx{zutwvqps"
-
      1  1:                                                 73                's'
      2  2:                                               4f73               'Os'
      3  3:                                             204f73              ' Os'
@@ -65,6 +15,25 @@
     14 14:                       652053717565616d697368204f73   'e Squeamish Os'
     15 15:                     72652053717565616d697368204f73  're Squeamish Os'
     16 16:                   6172652053717565616d697368204f73 'are Squeamish Os'
+
+    1::  1  1:                                                 20                ' '
+     2::  2  2:                                               7320               's '
+     3::  3  3:                                             647320              'ds '
+     4::  4  4:                                           72647320             'rds '
+     5::  5  5:                                         6f72647320            'ords '
+     6::  6  6:                                       576f72647320           'Words '
+     7::  7  7:                                     20576f72647320          ' Words '
+     8::  8  8:                                   6320576f72647320         'c Words '
+     9::  9  9:                                 696320576f72647320        'ic Words '
+    10:: 10 10:                               67696320576f72647320       'gic Words '
+    11:: 11 11:                             6167696320576f72647320      'agic Words '
+    12:: 12 12:                           4d6167696320576f72647320     'Magic Words '
+    13:: 13 13:                         204d6167696320576f72647320    ' Magic Words '
+    14:: 14 14:                       65204d6167696320576f72647320   'e Magic Words '
+    15:: 15 15:                     6865204d6167696320576f72647320  'he Magic Words '
+    16:: 16 16:                   546865204d6167696320576f72647320 'The Magic Words '
+    --------------------------------------------------------------------------------
+    The Magic Words are Squeamish Ossifrage\t\t\t\t\t\t\t\t\t
 
 """
 
@@ -101,51 +70,65 @@ def query(q):
         return good, e.code
     print('-' * 80)
     print(r)
-    assert False, "Can't happen"
+    assert False
 
 base = 'f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4'
 b = len(base)
 assert b % 2 == 0, b
 n = b // 2
 cip = [int(base[2 * i:2 * i + 2], 16) for i in range(n)]
-# print(n)
-# print(base)
-# print(txt(cip))
+print('cip=%d' % len(cip))
 assert txt(cip) == base
-# query(txt(cip))
-N = 48
-M = N - 16
-cip = cip[:N]
-# print(query(txt(cip)))
-# assert False
 
-answer = []
-for pos in range(1, M + 1):
-    found = False
-    print('%2d::' % pos, end=' ')
-    for n in range(0x100):
-        o = M - pos
-        # d = [n ^ cip[i] ^ pos for i in range(o, M)]
-        # h = list(reversed(answer + [n]))
-        h = [n] + list(reversed(answer))
-        # h = answer + [n]
-        # d = [h[i - o] ^ cip[i] ^ pos for i in range(o, M)]
-        d = [h[i] ^ cip[M - pos + i] ^ pos for i in range(pos)]
-        # d = [h[i] ^ cip[M - 1 - i] ^ pos for i in range(pos)]
-        xcip = cip[:M - pos] + d + cip[M:]
-        assert len(xcip) == N, len(xcip)
-        q = txt(xcip)
-        g, k = query(q)
-        # print('0x%02x 0x%02x %d - %s %s - %d %s' % (pos, n, o, txt(cip[o:32]), txt(d), k, g))
-        if g:
-            assert not found
-            found = True
+
+def do_block(cip, B):
+    N = (B + 1) * 16
+    M = B * 16
+    cip = cip[:N]
+    assert len(cip) == N
+    answer = []
+    plain = ''
+    for pos in range(1, 16 + 1):
+        found = False
+        orig_g = None
+        print('%2d::' % pos, end=' ')
+        for n in range(0x100):
+            o = M - pos
+            h = [n] + list(reversed(answer))
+            d = [h[i] ^ cip[M - pos + i] ^ pos for i in range(pos)]
+            xcip = cip[:M - pos] + d + cip[M:]
+            assert len(xcip) == N, (len(xcip), N, M, pos)
+            q = txt(xcip)
+            try:
+                g, k = query(q)
+            except:
+                orig_n = n
+                g = False
+            if g:
+                assert not found
+                found = True
+                answer.append(n)
+                a = list(reversed(answer))
+                plain = asc(a)
+                print('%2d %2d: %50s %18r' % (len(a), len(plain), txt(a), plain))
+                break
+            # assert k == 403 or g
+        if not found:
+            assert orig_n is not None
+            n = orig_n
             answer.append(n)
             a = list(reversed(answer))
-            print('%2d %2d: %50s %18r' % (len(a), len(asc(a)), txt(a), asc(a)))
-            # for _ in range(3):
-            #     g, _ = query(q)
-            #     assert g
-            break
-        assert k == 403 or g
-    assert found
+            plain = asc(a)
+            print('%2d %2d: %50s %18r' % (len(a), len(plain), txt(a), plain))
+
+    return plain
+
+
+answers = []
+for B in 1, 2, 3:
+    a = do_block(cip, B)
+    print('-' * 80)
+    answers.append(a)
+print('=' * 80)
+plain = ''.join(answers)
+print('%d %r' % (len(plain), plain))
